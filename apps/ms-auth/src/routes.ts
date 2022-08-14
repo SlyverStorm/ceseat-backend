a7cd61c1e7219a24248d56d0c06d65bfae5a780f
Gabriel RICARD <44494841+SlyverStorm@users.noreply.github.com>
2022-08-14 22:16:55 +0200

    build: update Jenkinsfile

--- apps/ms-auth/src/routes.ts
+++ apps/ms-auth/src/routes.ts
@@ -64,7 +64,7 @@ function routes(app: Express) {
 
     //Health check route :
     app.get("/healthcheck", (req: Request, res: Response) => {
-        res.sendStatus(200);
+        res.status(200).send("Authentification microservice working !");
     })
 }
 
